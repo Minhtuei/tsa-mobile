2d72725810bef2ef8cfd5e585ed75f5a37c028a4
Minhtuei <tue.tran20032003@gmail.com>
2024-10-13 21:35:36 +0700

    refactor: change animation

--- app/pages/home/DashBoard.tsx
+++ app/pages/home/DashBoard.tsx
@@ -8,7 +8,7 @@ import { Platform, ScrollView, View, Animated } from 'react-native';
 import { Text } from 'react-native-paper';
 import BackgroundIcon from '../../../assets/background-icon.svg';
 import QueryTypeBtnTab from '@components/QueryTypeBtnTab';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 export const Dashboard = () => {
   const HIDE_TAB_HEIGHT = Platform.OS === 'ios' ? 100 : 50;
 
@@ -16,15 +16,21 @@ export const Dashboard = () => {
   const [selectedType, setSelectedType] = useState<
     'today' | 'yesterday' | 'week' | 'month'
   >('today');
-  const scrollY = new Animated.Value(0);
+  const scrollY = useRef(new Animated.Value(0)).current;
+  const scrollViewRef = useRef<ScrollView>(null);
   const millipedeOpacity = scrollY.interpolate({
     inputRange: [0, DASHBOARD_HEADER_HEIGHT / 2],
     outputRange: [1, 0],
     extrapolate: 'clamp',
   });
   const stickyTop = scrollY.interpolate({
-    inputRange: [DASHBOARD_HEADER_HEIGHT, DASHBOARD_HEADER_HEIGHT * 2],
+    inputRange: [DASHBOARD_HEADER_HEIGHT, DASHBOARD_HEADER_HEIGHT * 1.5],
     outputRange: [-HIDE_TAB_HEIGHT, 0],
+    extrapolate: 'clamp',
+  });
+  const stickyOpacity = scrollY.interpolate({
+    inputRange: [DASHBOARD_HEADER_HEIGHT, DASHBOARD_HEADER_HEIGHT * 1.5],
+    outputRange: [0, 1],
     extrapolate: 'clamp',
   });
   const InfoCardAnimation = {
@@ -41,6 +47,7 @@ export const Dashboard = () => {
   return (
     <View style={[globalStyles.background]}>
       <ScrollView
+        ref={scrollViewRef}
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 24, flexGrow: 1 }}
         onScroll={Animated.event(
@@ -71,66 +78,14 @@ export const Dashboard = () => {
           </Text>
           <BackgroundIcon width={SCREEN.width} height={SCREEN.height * 0.3} />
         </View> */}
-        <Text
-          style={[globalStyles.title, { marginTop: 24, textAlign: 'center' }]}
-        >
-          Bạn chưa có đơn hàng nào! Hãy tạo đơn hàng đầu tiên của bạn
-        </Text>
-        <Text
-          style={[globalStyles.title, { marginTop: 24, textAlign: 'center' }]}
-        >
-          Bạn chưa có đơn hàng nào! Hãy tạo đơn hàng đầu tiên của bạn
-        </Text>
-        <Text
-          style={[globalStyles.title, { marginTop: 24, textAlign: 'center' }]}
-        >
-          Bạn chưa có đơn hàng nào! Hãy tạo đơn hàng đầu tiên của bạn
-        </Text>
-        <Text
-          style={[globalStyles.title, { marginTop: 24, textAlign: 'center' }]}
-        >
-          Bạn chưa có đơn hàng nào! Hãy tạo đơn hàng đầu tiên của bạn
-        </Text>
-        <Text
-          style={[globalStyles.title, { marginTop: 24, textAlign: 'center' }]}
-        >
-          Bạn chưa có đơn hàng nào! Hãy tạo đơn hàng đầu tiên của bạn
-        </Text>
-        <Text
-          style={[globalStyles.title, { marginTop: 24, textAlign: 'center' }]}
-        >
-          Bạn chưa có đơn hàng nào! Hãy tạo đơn hàng đầu tiên của bạn
-        </Text>
-        <Text
-          style={[globalStyles.title, { marginTop: 24, textAlign: 'center' }]}
-        >
-          Bạn chưa có đơn hàng nào! Hãy tạo đơn hàng đầu tiên của bạn
-        </Text>
-        <Text
-          style={[globalStyles.title, { marginTop: 24, textAlign: 'center' }]}
-        >
-          Bạn chưa có đơn hàng nào! Hãy tạo đơn hàng đầu tiên của bạn
-        </Text>
-        <Text
-          style={[globalStyles.title, { marginTop: 24, textAlign: 'center' }]}
-        >
-          Bạn chưa có đơn hàng nào! Hãy tạo đơn hàng đầu tiên của bạn
-        </Text>
-        <Text
-          style={[globalStyles.title, { marginTop: 24, textAlign: 'center' }]}
-        >
-          Bạn chưa có đơn hàng nào! Hãy tạo đơn hàng đầu tiên của bạn
-        </Text>
-        <Text
-          style={[globalStyles.title, { marginTop: 24, textAlign: 'center' }]}
-        >
-          Bạn chưa có đơn hàng nào! Hãy tạo đơn hàng đầu tiên của bạn
-        </Text>
-        <Text
-          style={[globalStyles.title, { marginTop: 24, textAlign: 'center' }]}
-        >
-          Bạn chưa có đơn hàng nào! Hãy tạo đơn hàng đầu tiên của bạn
-        </Text>
+        {Array.from({ length: 12 }).map((_, index) => (
+          <Text
+            key={index}
+            style={[globalStyles.title, { marginTop: 24, textAlign: 'center' }]}
+          >
+            Bạn chưa có đơn hàng nào! Hãy tạo đơn hàng đầu tiên của bạn
+          </Text>
+        ))}
       </ScrollView>
       <Animated.View
         style={{
@@ -141,7 +96,7 @@ export const Dashboard = () => {
           top: stickyTop,
           left: 0,
           right: 0,
-          opacity: 1,
+          opacity: stickyOpacity,
           height: HIDE_TAB_HEIGHT,
           position: 'absolute',
           ...Platform.select({
